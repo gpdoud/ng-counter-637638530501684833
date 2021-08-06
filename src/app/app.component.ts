0ca02a37e7b5100f51f2caf298eb8a06617bb303
Greg Doud <gpdoud@gmail.com>
2021-08-06 13:24:11 -0400

    Committed by GitUtil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-counter';
+  nbr: number = 0;
+  inpCls: string = "";
+
+  change(amount: number): void {
+    this.nbr += amount;
+    this.display();
+  }
+  display(): void {
+    this.inpCls = "";
+    if(this.nbr % 2 == 0) this.inpCls += " text-danger ";
+    if(this.nbr % 3 == 0) this.inpCls += " fw-bold ";
+    if(this.nbr % 7 == 0) this.inpCls += " fst-italic ";
+  }
 }
